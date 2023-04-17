dcf4ad3d0b5fa6e6e1e6ff77437faa4c942be874
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2023-04-18 00:17:17 +0900

    gateway init setting

---
+++ src/test/java/com/example/chieegateway/ChieegatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chieegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChieegatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
