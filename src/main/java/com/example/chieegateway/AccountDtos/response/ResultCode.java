2f6610241bdc65c0745d9233cca297b12c088092
Identityless <rlandwnd555@naver.com>
2023-08-31 17:41:01 +0900

    feat : 스트리밍 관련 응답 코드 추가

--- src/main/java/com/example/chieegateway/AccountDtos/response/ResultCode.java
+++ src/main/java/com/example/chieegateway/AccountDtos/response/ResultCode.java
@@ -33,7 +33,21 @@ public enum ResultCode {
   KEYWORD_UPDATE_SUCCESS("K002", "키워드 수정 성공"),
   KEYWORD_DELETE_SUCCESS("K003", "키워드 삭제 성공"),
   KEYWORD_FIND_SUCCESS("K004", "키워드 조회 성공"),
-  KEYWORD_LIST_REQUEST_SUCCESS("K005", "키워드 리스트 조회 성공");
+  KEYWORD_LIST_REQUEST_SUCCESS("K005", "키워드 리스트 조회 성공"),
+
+  //VODGroup
+  VODGROUP_REGISTRATION_SUCCESS("V001", "VOD 그룹 등록 성공"),
+  VODGROUP_UPDATE_SUCCESS("V002", "VOD 그룹 수정 성공"),
+  VODGROUP_SEARCH_SUCCESS("V003", "VOD 그룹 조회 성공"),
+  VODGROUP_DELETE_SUCCESS("V004", "VOD 그룹 삭제 성공"),
+  VODGROUP_LIST_REQUEST_SUCCESS("V005", "VOD 그룹 리스트 조회 성공"),
+
+  //VOD
+  VOD_REGISTRATION_INIT_SUCCESS("V006", "VOD 등록 초기화 성공"),
+  VOD_MULTIPART_UPLOAD_SUCCESS("V007", "VOD 멀티파트 업로드 성공"),
+  VOD_UPLOAD_COMPLATE("V007", "VOD 업로드 완료"),
+  VOD_UPLOAD_FINISH_SUCCESS("V008", "VOD 업로드 완료 요청 성공")
+  ;
 
   private final String code;
   private final String message;
